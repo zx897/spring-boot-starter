60b24289abdcb582fcc0fce8daca0fe73eb3ec2b
Qian Zhang <897157658@qq.com>
2022-10-14 10:35:36 +0800

    fix : fix the set and get  and construction  function to loombook's setter and getter and construction annotications  .

--- src/main/java/cn/thoughtworks/school/entities/Company.java
+++ src/main/java/cn/thoughtworks/school/entities/Company.java
@@ -1,10 +1,16 @@
 package cn.thoughtworks.school.entities;
 
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.Set;
 
 @Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Company {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,34 +21,4 @@ public class Company {
     @JoinColumn(name = "companyId")
     private Set<Employee> employees;
 
-    public Company() {
-    }
-
-    public Company(Long id) {
-        this.id = id;
-    }
-
-    public  Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getCompanyName() {
-        return companyName;
-    }
-
-    public void setCompanyName(String companyName) {
-        this.companyName = companyName;
-    }
-
-    public Set<Employee> getEmployees() {
-        return employees;
-    }
-
-    public void setEmployees(Set<Employee> employees) {
-        this.employees = employees;
-    }
 }
--- src/main/java/cn/thoughtworks/school/entities/Employee.java
+++ src/main/java/cn/thoughtworks/school/entities/Employee.java
@@ -1,12 +1,18 @@
 package cn.thoughtworks.school.entities;
 
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,52 +22,4 @@ public class Employee {
     private String gender;
     private Long companyId;
 
-    public Employee() {
-    }
-
-    public Employee(String name, Integer age, String gender,  Long companyId) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-        this.companyId = companyId;
-    }
-
-    public Long getCompanyId() {
-        return companyId;
-    }
-
-    public void setCompanyId(Long companyId) {
-        this.companyId = companyId;
-    }
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Employee setName(String name) {
-        this.name = name;
-        return this;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
 }
